bb55a0831a3cc61aa2da54f402d66a5027360579
ismailcanaydin <ismailcanaydin@gmail.com>
2022-10-01 18:01:26 +0300

    React-Intl

--- src/react-initl/React_Inıtl.js
+++ src/react-initl/React_Inıtl.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import { useState } from 'react';
+import { IntlProvider, FormattedMessage, FormattedNumber } from 'react-intl';
+import styles from '../App.css';
+const messages = {
+  'tr-TR': {
+    title: 'Merhaba Dünya',
+    description: '3 yeni mesajınız var!'
+  },
+  'en-EN': {
+    title: 'Hello World',
+    description: 'You have 3 new messages!',
+  }
+};
 
 function React_Inıtl() {
+  const [lang, setLang] = useState('tr-TR');
+
   return (
-    <div>React_Inıtl</div>
+    <div>
+      <IntlProvider messages={messages[lang]}>
+        <FormattedMessage id='title' />
+        <p>
+          <FormattedMessage id='description' />
+        </p>
+
+        <br />
+        <br />
+        <button onClick={() => setLang('tr-TR')}>TR</button>&nbsp;
+        <button onClick={() => setLang('en-EN')}>EN</button>
+      </IntlProvider>
+    </div>
   )
 }
 
